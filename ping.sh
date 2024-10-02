622cc8e22b7626178dd0d11178972692bc16f63f
sirmosi <mo.sharbaf@gmail.com>
2024-10-02 20:24:03 +0330

    first

---
+++ ping.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+    then
+        echo "You forgot to enter ip address"
+        echo "Syntax: ./ping.sh 192.168.1"
+    else
+      for ip in `seq 1 254`:
+          do
+             ping -c 1 $1.$ip | sed -n 's/.*from \([0-9.]*\).*/\1/p'
+          done
+fi
